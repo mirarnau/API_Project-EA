30c35b276b702dff4f1170205e14d650624e39ee
eduardferre <eduard.ferre.sanchez@estudiantat.upc.edu>
2022-05-14 04:20:50 +0200

    docker

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import helmet from 'helmet';
 import mongoose from 'mongoose';
 import compression from 'compression';
 import cors from 'cors';
+import dotenv from "dotenv";
 
 import indexRoutes from './routes/indexRoutes';
 import customersRoutes from './routes/customersRoutes';
@@ -12,8 +13,14 @@ import restaurantsRoutes from './routes/restaurantsRoutes';
 import reservationsRoutes from './routes/reservationsRoutes';
 import dishesRoutes from './routes/dishesRoutes';
 
+
+dotenv.config({ path: `.env.${process.env.NODE_ENV || "development"}` });
+dotenv.config({ path: ".env.secret" });
+
 class Server {
     public app: express.Application;
+
+    
 
     //The contructor will be the first code that is executed when an instance of the class is declared.
     constructor(){
@@ -24,7 +31,11 @@ class Server {
 
     config() {
         //MongoDB settings
-        let DB_URL = process.env.DB_URL || 'mongodb://localhost/EA-PROJECT';
+        let DB_URL = process.env.DB_URL || 'mongodb://localhost:27017/EA-PROJECT';
+
+        DB_URL = DB_URL.replace("user", process.env.DB_USER!);
+        DB_URL = DB_URL.replace("password", process.env.DB_PASSWORD!);
+
         mongoose.connect(DB_URL).then(db => console.log("DB is connected"));
 
         //Settings
